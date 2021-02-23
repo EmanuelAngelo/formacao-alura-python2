9bce6fc7d4bd538629ebcf9599556a15c392cdcb
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-02-23 15:27:24 -0300

    usando manipuladores de string

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,24 +3,34 @@ def jogar():
     print('****** Bem-vindo ao jogo da Forca *******')
     print('*****************************************')
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
+
     print(letras_acertadas)
     while (not acertou and not enforcou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
-
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        chute = chute.strip().upper()
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-
+    if (acertou):
+        print("Voce ganhou, a palavra {} está correta.!!".format(palavra_secreta))
+    else:
+        print("Perdeu, palavra errada.")
     print("Fim do jogo")
 
 if(__name__ == '__main__'):
